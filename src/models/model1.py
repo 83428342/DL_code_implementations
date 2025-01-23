1e58dfb2f894fac38da68bb713f7a98a7f6b9972
83428342 <bill8342@cau.ac.kr>
2025-01-24 04:59:55 +0900

    model2 추가

--- src/models/model1.py
+++ src/models/model1.py
@@ -1,10 +1,7 @@
-import sys
-import os
+import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-
 import numpy as np
 import matplotlib.pyplot as plt
-
 from layers.activations import *
 
 def init_network():
