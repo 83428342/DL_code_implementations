47676bb0c0f6cb056313c109d30e7ba3f913630a
83428342 <bill8342@cau.ac.kr>
2025-01-23 23:37:33 +0900

    폴더 구조 개선

--- src/models/model1.py
+++ src/models/model1.py
@@ -1,38 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import sys, os
+import sys
+import os
+
+from layers.activation_function import *
 from data.mnist import load_mnist
-
-# step function
-
-def step_function(x):
-    y = x > 0
-    return y.astype(int) # boolean 값의 0, 1 반환
-
-# sigmoid
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-# relu
-
-def relu(x):
-    return np.maximum(0, x)
-
-# identity function
-
-def identity_function(x):
-    return x
-
-# softmax
-
-def softmax(a):
-    c = np.max(a) # overflow 방지
-    exp_a = np.exp(a - c)
-    sum_exp_a = np.sum(exp_a)
-    y = exp_a / sum_exp_a
-    
-    return y
 
 def init_network():
     network = {}
